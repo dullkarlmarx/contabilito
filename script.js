014c02d5718c4669f35d740d50c7466274c153eb
christiansanguinetti <christiansanguinetti882@gmail.com>
2022-08-17 19:45:35 -0300

    agregue comentario

---
+++ script.js
@@ -0,0 +1,6 @@
+/*de Agustin Vazquez a todos en esta sesión:    7:44 PM
+To add new rows and cells to an HTML table in Javascript:
+Get the table – var table = document.getElementById("TABLE");
+Add a new row – var row = table.insertRow();
+Append the cells – var cell = row.insertCell();
+Set cell content – cell.innerHTML = "TEXT";
